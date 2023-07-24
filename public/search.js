adae56406a709d92084ce3c94831c45a94f24c3c
Mike Rabayda <mrabayda4@icloud.com>
2023-07-24 19:38:21 -0400

    all types searchable for spotify id

--- public/search.js
+++ public/search.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Configure the search bar using Select2
     $('#search-bar').select2({
         placeholder: 'Enter your search term',
@@ -10,13 +10,48 @@ $(document).ready(function() {
             data: function (params) {
                 return {
                     q: params.term, // search term
-                    type: $('#search-type').val() // search type
+                    type: $('#search-type').val() // search type - track, artist, album, playlist, etc.
                 };
             },
             processResults: function (data) {
+                // return {
+                //     results: data.tracks.items.map(item => {
+                //         const text = $('#search-type').val() === 'track' ? `${item.name} by ${item.artists[0].name}` : item.name;
+                //         return {
+                //             id: item.id,
+                //             text: text
+                //         };
+                //     })
+                // };
+                let items = [];
+                if ($('#search-type').val() === 'track') {
+                    items = data.tracks.items;
+                } 
+                else if ($('#search-type').val() === 'artist') {
+                    items = data.artists.items;
+                }
+                else if ($('#search-type').val() === 'album') {
+                    items = data.albums.items;
+                }
+                else if ($('#search-type').val() === 'playlist') {
+                    items = data.playlists.items;
+                }
+                else if ($('#search-type').val() === 'show') {
+                    items = data.shows.items;
+                }
+                else if ($('#search-type').val() === 'episode') {
+                    items = data.episodes.items;
+                }
+                else if ($('#search-type').val() === 'audiobook') {
+                    items = data.audiobooks.items;
+                }
+
                 return {
-                    results: data.tracks.items.map(item => {
-                        const text = $('#search-type').val() === 'track' ? `${item.name} by ${item.artists[0].name}` : item.name;
+                    results: items.map(item => {
+                        const text = $('#search-type').val() === 'track'
+                            ? `${item.name} by ${item.artists[0].name}`
+                            : item.name;
+
                         return {
                             id: item.id,
                             text: text
