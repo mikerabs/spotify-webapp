5f3d1287bd5759633384390916c2bef92112fb99
Mike Rabayda <mrabayda4@icloud.com>
2023-07-24 19:17:28 -0400

    spotify id returned in search

--- public/search.js
+++ public/search.js
@@ -1,53 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Function to handle the search
-    async function handleSearch() {
-        const searchTerm = document.getElementById('search-term').value;
-        const searchType = document.getElementById('search-type').value;
+$(document).ready(function() {
+    // Configure the search bar using Select2
+    $('#search-bar').select2({
+        placeholder: 'Enter your search term',
+        minimumInputLength: 2,
+        ajax: {
+            url: '/search',
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+                return {
+                    q: params.term, // search term
+                    type: $('#search-type').val() // search type
+                };
+            },
+            processResults: function (data) {
+                return {
+                    results: data.tracks.items.map(item => {
+                        const text = $('#search-type').val() === 'track' ? `${item.name} by ${item.artists[0].name}` : item.name;
+                        return {
+                            id: item.id,
+                            text: text
+                        };
+                    })
+                };
+            },
+            cache: true
+        },
+    });
 
-        try {
-            // Make an API call to your server to perform the search
-            const response = await fetch(`/search?q=${encodeURIComponent(searchTerm)}&type=${searchType}`);
-            const data = await response.json();
+    // Show detailed info when a result is selected
+    $('#search-bar').on('select2:select', function (e) {
+        const data = e.params.data;
+        $('#detail-name').text(`Name: ${data.text}`);
+        $('#detail-spotify-id').text(`Spotify ID: ${data.id}`);
+        $('#detailed-info').show();
+    });
 
-            // Render the search results on the page
-            const searchResultsDiv = document.getElementById('search-results');
-            searchResultsDiv.innerHTML = '';
-
-            if (searchType === 'track') {
-                data.tracks.items.forEach(track => {
-                    searchResultsDiv.innerHTML += `<p>${track.name} - ${track.artists[0].name}</p>`;
-                });
-            } else if (searchType === 'artist') {
-                data.artists.items.forEach(artist => {
-                    searchResultsDiv.innerHTML += `<p>${artist.name}</p>`;
-                });
-            } else if (searchType === 'album') {
-                data.albums.items.forEach(album => {
-                    searchResultsDiv.innerHTML += `<p>${album.name} - ${album.artists[0].name}</p>`;
-                });
-            } else if (searchType === 'playlist') {
-                data.playlists.items.forEach(playlist => {
-                    searchResultsDiv.innerHTML += `<p>${playlist.name}</p>`;
-                });
-            } else if (searchType === 'show') {
-                data.shows.items.forEach(show => {
-                    searchResultsDiv.innerHTML += `<p>${show.name}</p>`;
-                });
-            } else if (searchType === 'episode') {
-                data.episodes.items.forEach(episode => {
-                    searchResultsDiv.innerHTML += `<p>${episode.name} - ${episode.show.name}</p>`;
-                });
-            } else if (searchType === 'audiobook') {
-                data.audiobooks.items.forEach(audiobook => {
-                    searchResultsDiv.innerHTML += `<p>${audiobook.name}</p>`;
-                });
-            }
-
-        } catch (error) {
-            console.error('Error performing search:', error);
-        }
-    }
-
-    // Event listener for the search button
-    document.getElementById('search-button').addEventListener('click', handleSearch);
+    // Clear detailed info when the selection is cleared
+    $('#search-bar').on('select2:unselect', function (e) {
+        $('#detailed-info').hide();
+    });
 });
